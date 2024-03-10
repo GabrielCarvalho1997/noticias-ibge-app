49a8a43bdf0262f112682fc25de8a74d3b9faae8
Gabriel Carvalho <gabrielcoelhoneves@hotmail.com>
2024-03-10 17:25:22 -0300

    configs

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 // Principais constante do projeto
-export const API_URL = "http://servicodados.ibge.gov.br/api/v3/noticias/";
+export const API_URL = "https://servicodados.ibge.gov.br/api/v3/noticias/";
 export const API_URL_IMAGE = "https://agenciadenoticias.ibge.gov.br/";
 
 export const SITE_IBGE = "https://agenciadenoticias.ibge.gov.br/";
